bcd6150f55b4643f2ce198f0e91390461b9879c4
Roman-Zajic <zajic.roman.mobile@gmail.com>
2022-12-11 11:25:44 +0100

    first commit

---
+++ Algorithms.py
@@ -0,0 +1,8 @@
+"""
+The library contains most common Algorithms and data structure examples.
+
+Title:          Most Common Algorithms
+Author:         Roman Zajic
+Last modified:  2022-12-11
+"""
+
